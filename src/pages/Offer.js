c40bb776defd49ea97d7713b4bf7ba0fd262b0ff
Louise <grillonlouise.pro@gmail.com>
2022-08-07 18:37:38 +0200

    commit

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import "../css/offer.scss";
 import { Link } from "react-router-dom";
 
 // price
@@ -35,33 +35,38 @@ const Offer = () => {
   return isLoading ? (
     <span>En cours de chargement...</span>
   ) : (
-    <div className="item-container">
-      <img className="img-offer" src={data.product_image.secure_url} alt="" />
-      <div className="item-info">
-        <p>{data.product_price}</p>
+    <div className="offer-main">
+      <div className="offer-container">
+        <img className="img-offer" src={data.product_image.secure_url} alt="" />
+        <div className="info-container">
+          <div className="price">
+            <p>{data.product_price}</p>
+          </div>
 
-        {data.product_details.map((elem, index) => {
-          const keys = Object.keys(elem);
-          return (
-            <div className="product-info">
-              <p key={index}>
-                {keys[0]} : {elem[keys[0]]}
-              </p>
-            </div>
-          );
-        })}
-        <div className="div-border"></div>
-        <div className="div2">
-          <p>{data.product_name}</p>
+          <div className="product-info">
+            {data.product_details.map((elem, index) => {
+              const keys = Object.keys(elem);
+              return (
+                <div>
+                  <p key={index}>{keys[0]}</p>
+                  <p>{elem[keys[0]]}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="div-border"></div>
+          <div className="div2">
+            <p className="p1">{data.product_name}</p>
 
-          <p className="p2">{data.product_description}</p>
-          <p>{data.owner.account.username}</p>
-          <Link
-            to="/payment"
-            state={{ title: data.product_name, price: data.product_price }}
-          >
-            Acheter
-          </Link>
+            <p className="p2">{data.product_description}</p>
+            <p>{data.owner.account.username}</p>
+            <Link
+              to="/payment"
+              state={{ title: data.product_name, price: data.product_price }}
+            >
+              Acheter
+            </Link>
+          </div>
         </div>
       </div>
     </div>
